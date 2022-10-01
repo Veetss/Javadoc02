309e10eb38c3160fddd2d651aae311c04a32122e
Vittoria Vassarotti <vit.vassarotti@outlook.it>
2022-10-01 10:35:51 +0200

    Initial commit

---
+++ GeneratedJavadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ArithmeticOperators","l":"ArithmeticOperators()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Tester","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Tester","l":"Tester()","u":"%3Cinit%3E()"}];updateSearchResults();
