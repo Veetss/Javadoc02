50362323c6280f40129c521f68295c9f35364e4f
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:17:16 +0100

    Update ArithmeticOperators.java

--- src/ArithmeticOperators.java
+++ src/ArithmeticOperators.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class ArithmeticOperators {
+/**
+ * Questa è la classe in cui definisco il valore delle mie variabili e i metodi da
+ * richiamare nel main per poter svolgere i calcoli richiesti.
+ *
+ * @author Vittoria Vassarotti
+ */
+
+public class ArithmeticOperators {
+
+    static int num1 = 25;
+    static int num2 = 5;
+
+    /**
+     * Metodo richiesto per stampare la SOMMA di num1, num2. 25+5=30.
+     */
+    static void sum() {
+        System.out.println(num1 + num2);
+    }
+
+    /**
+     * Metodo richiesto per stampare la SOTTRAZIONE di num1, num2. 25-5=20.
+     */
+    static void sub() {
+        System.out.println(num1 - num2);
+    }
+
+    /**
+     * Metodo richiesto per stampare la MOLTIPLICAZIONE di num1, num2. 25*5=125.
+     */
+    static void mul() {
+        System.out.println(num1 * num2);
+    }
+
+    /**
+     * Metodo richiesto per stampare la DIVISIONE di num1, num2. 25/5=5.
+     */
+    static void div() {
+        System.out.println(num1 / num2);
+    }
 }
