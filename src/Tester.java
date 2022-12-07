bdccef811ca1207a9b0927229151b8f909e9b004
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:17:37 +0100

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Tester {
+/**
+ * Questa la mia classe main in cui andrò a stampare i risultati dei calcoli richiesti.
+ *
+ * @author Vittoria Vassarotti
+ */
+
+public class Tester {
+
+    /**
+     * Metodo MAIN con il quale vado a stampare tutte le richieste poste dall'esercizio
+     * richiamando quindi i metodi della classe ArithmeticOperators (per poter appunto
+     * stampare i risultati dei calcoli).
+     * @param args
+     */
+    public static void main(String[] args) {
+        ArithmeticOperators arithmeticOperators = new ArithmeticOperators();
+        ArithmeticOperators.sum();
+        ArithmeticOperators.sub();
+        ArithmeticOperators.mul();
+        ArithmeticOperators.div();
+    }
 }
